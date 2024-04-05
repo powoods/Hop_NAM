9a18413e5038507c16cd9af39890ffbf0ae3066e
Patrick Woods <patrickodwoods@gmail.com>
2024-04-05 12:38:22 -0600

    adding an additional parameter to fastp command.

--- NAM_parent_fastq_processing.sh
+++ NAM_parent_fastq_processing.sh
@@ -22,7 +22,7 @@ r2=${f/_L003-L004_R1_001.fastq.gz}_L003-L004_R2_001.fastq.gz
 r3=${f/_L003-L004_R1_001.fastq.gz}_R1_trim.fastq.gz
 r4=${f/_L003-L004_R1_001.fastq.gz}_r2_trim.fastq.gz
 r5=${f/_L003-L004_R1_001.fastq.gz}_report.html
-echo  "fastp --thread 8 -p -l 36 -g --cut_front --cut_tail -y --detect_adapter_for_pe -h $r5 -i $f -I $r2 -o $r3 -O $r4"
+echo  "fastp --thread 8 -p -l 36 -g --cut_front --cut_tail -y --detect_adapter_for_pe --adapter_fasta adapters.fa -h $r5 -i $f -I $r2 -o $r3 -O $r4"
 done
 
 # Use fastqc as a secondary check to make sure parent fastq.gz files look good for alignment #
